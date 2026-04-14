43b34f8f1c9879fcfac9a161d0c7f7e4b9ff17d3
rezh1808 <rezahadi26@gmail.com>
2026-04-14 15:36:56 +0700

    Change email provider

--- src/main/java/com/bank/EmailService.java
+++ src/main/java/com/bank/EmailService.java
@@ -1,6 +1,7 @@
 package com.bank;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,22 +12,30 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendOtpEmail(String toEmail, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Your NyuboBank Verification Code");
-        message.setText("Welcome to NyuboBank! Your 2FA code is: " + otp +
-                "\n\nPlease enter this code to verify your account.");
+    // This pulls your email from properties so it always matches your login
+    @Value("${spring.mail.username}")
+    private String fromEmail;
 
-        mailSender.send(message);
-    }
+    /**
+     * Standard method to send any text email
+     */
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("yourbank@mailtrap.io");
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
+
         mailSender.send(message);
     }
 
+    /**
+     * Specific method for OTPs (Optional, but looks cleaner in your AuthController)
+     */
+    public void sendOtpEmail(String to, String otp) {
+        String subject = "Your NyuboBank Verification Code";
+        String body = "Halo!\n\nYour verification code is: " + otp +
+                "\n\nIf you did not request this, please ignore this email.";
+        sendEmail(to, subject, body);
+    }
 }
